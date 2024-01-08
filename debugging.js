b2500a0956bb1a3d918cca4663a17683661d5606
dragnum3xp <320235583@byui.edu>
2024-01-08 09:05:42 -0300

    learning activities for week 1

---
+++ debugging.js
@@ -0,0 +1,14 @@
+const pi = 3.14;
+let radius = 3;
+let area = 0;
+function circleArea(radius) {
+    let area = radius * radius * pi;
+    return area;
+  }
+area = circleArea(3);
+console.log("Area1:", area);
+radius = 4;
+area = circleArea(4);
+console.log("Area2:", area);
+
+
